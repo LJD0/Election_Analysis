a9be47ff4fb4969aafa014b5375abef7cca7c12c
OAO-MadHatter <larsenj2064@gmail.com>
2022-09-17 13:25:26 -0700

    3.5.2 commit added Candidate list

--- PyPoll.py
+++ PyPoll.py
@@ -1,11 +1,25 @@
 import csv as c
 import os
+from unittest import skip
 
 file_to_load = os.path.join("Resources", "election_results.csv")
 
 file_to_save = os.path.join("Results", "election_results.txt")
 
-with open(file_to_load) as Data1:
-    file_reader = c.reader(Data1)
+total_votes = 0
+
+candidate_options = []
+
+
+with open(file_to_load) as election_data:
+    file_reader = c.reader(election_data)
+
+    header = next(election_data)
+
     for row in file_reader:
-        print(row)
+        total_votes += 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+
+print(candidate_options)
